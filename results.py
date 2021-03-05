ca2ab68c9e008bdea1d2ef0fcab02f46138c5a96
erikpu <erikpuijk@gmail.com>
2021-03-05 11:32:26 +0100

    Confirm button and page loads working

--- results.py
+++ results.py
@@ -7,41 +7,48 @@ import plotly.express as px
 from industries_to_companies import get_companies_from_industries
 from financial_data import financial_data
 #from Get_ESGScores_FromList import get_esg_scores
-from YFinance_Function.py import get_ESG
+from YFinance_Function import get_ESG
 
 
-def app(selected_industries, container):
-    industry=selected_industries # put in selected industries
+def app(selected_industries, container, status):
 
-    if industry:
-        container.write(industry)
-        companies = get_companies_from_industries(industry).tolist()
-    
-    container.write(companies)
-    #print(companies, type(companies))
-    
-    #df2 = financial_data(companies)
-    #print(df2)
+    # Only run this if the app is in the results phase
+    if status == 1:
+
+        # Introduction
+        container.write('Find below your results')
+
+        industry=selected_industries # put in selected industries
+
+        if industry:
+            container.write(industry)
+            companies = get_companies_from_industries(industry).tolist()
+        
+        container.write(companies)
+        #print(companies, type(companies))
+        
+        #df2 = financial_data(companies)
+        #print(df2)
 
 
-    df3 = get_ESG(companies)
-    print(df3)
+        #df3 = get_ESG(companies)
+        #print(df3)
 
-    container.write('Results (Top 10 of selected investements)')
+        container.write('Results (Top 10 of selected investements)')
 
-    # Hardcoded dataframes to simulate input from previous steps (To be removed)
-    df1 = pd.DataFrame({'ticker': ["APPL", "GOOGL", "MSFT"], 'overall_score': [98.7, 56.2, 76],
-                        'E_score': [10, 30, 20], 'S_score': [15, 30, 60], 'G_score': [80, 25, 38]},
-                       columns=['ticker', 'overall_score', "E_score", "S_score", "G_score"])
-    df2 = pd.DataFrame({'ticker': ["APPL", "GOOGL", "MSFT"], 'Price_per_share': [112, 78, 230]},
-                       columns=['ticker', 'Price_per_share'])
+        # Hardcoded dataframes to simulate input from previous steps (To be removed)
+        df1 = pd.DataFrame({'ticker': ["APPL", "GOOGL", "MSFT"], 'overall_score': [98.7, 56.2, 76],
+                            'E_score': [10, 30, 20], 'S_score': [15, 30, 60], 'G_score': [80, 25, 38]},
+                           columns=['ticker', 'overall_score', "E_score", "S_score", "G_score"])
+        df2 = pd.DataFrame({'ticker': ["APPL", "GOOGL", "MSFT"], 'Price_per_share': [112, 78, 230]},
+                           columns=['ticker', 'Price_per_share'])
 
-    # Combine the results and show in a table
-    df = combine_dfs(df1, df2, "ticker", 'overall_score')
-    container.table(df.style.format({'overall_score': '{:.2f}'}))
+        # Combine the results and show in a table
+        df = combine_dfs(df1, df2, "ticker", 'overall_score')
+        container.table(df.style.format({'overall_score': '{:.2f}'}))
 
-    container.write('Market Trends for the selected industries')
+        container.write('Market Trends for the selected industries')
 
-    fig = px.pie(df, values='overall_score', names='ticker')
-    fig.update_traces(textposition='inside', textinfo='percent+label')
-    container.plotly_chart(fig)
+        fig = px.pie(df, values='overall_score', names='ticker')
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        container.plotly_chart(fig)
--- webapp.py
+++ webapp.py
@@ -6,16 +6,36 @@ import results
 import sidebar
 import SessionState
 
+# Input title
 st.title('Sustainable Investment Service')
 
+# Create empty placeholders to put in results and questionnaire
 holder_results = st.empty()
 holder_questionnaire = st.empty()
 
+# Create containers in these holders for the controls
 container_questionnaire = holder_questionnaire.beta_container()
 container_results = holder_results.beta_container()
 
-session_state = SessionState.get(selected_industries=[])
-session_state.selected_industries = questionnaire.app(container_questionnaire)
-results.app(session_state.selected_industries, container_results)
+# Use the SessionState to save the list of selected industries and status
+# Status 0: questionnaire phase
+# Status 1: results phase
+session_state = SessionState.get(selected_industries=[], status=0)
 
-holder_questionnaire.empty()
+# Launch the questionnaire and save the returned industries in the session state
+# and save the new status (1) in session state so that the results are launched
+session_state.selected_industries, session_state.status = questionnaire.app(container_questionnaire, session_state.status)
+
+# Launch the results if the status is 1
+if session_state.status == 1:
+    # Launch the results and give the selected industries as argument
+    results.app(session_state.selected_industries, container_results, session_state.status)
+
+    # Empty the questionnaire placeholder to show the results placeholder
+    holder_questionnaire.empty()
+
+# Add disclaimer (TO DO)
+st.title('Disclaimer!')
+st.write('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. '
+         'Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+         ' Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. ')
